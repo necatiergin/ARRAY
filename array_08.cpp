4624f9849a8eba3160e7539c3a047c7ae85870ba
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-05-25 08:46:10 +0300

    Update array_08.cpp

--- array_08.cpp
+++ array_08.cpp
@@ -4,7 +4,7 @@
 template <typename T, size_t n>
 std::ostream& operator<<(std::ostream& os, const std::array<T, n>& ar)
 {
-	os << "[size: " << ar.size() << "] |";
+	os << '|';
 	for (size_t i{}; i < ar.size() - 1; ++i) {
 		os << ar[i] << ", ";
 	}
@@ -16,7 +16,7 @@ int main()
 	std::array<std::array<int, 3>, 4> ar{ {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}, {4, 4, 4}} };
 
 	ar[2][1] = 65;
-	ar[1][3] = 99;
+	ar[1][2] = 99;
 
-	std::cout << ar << "\n";
+	std::cout << ar << '\n';
 }
