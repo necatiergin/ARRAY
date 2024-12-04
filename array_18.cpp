bb0fdd09e8276a83f5b901fbc7451363d9698470
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-04 18:00:40 +0300

    Update array_18.cpp

--- array_18.cpp
+++ array_18.cpp
@@ -1,19 +1,16 @@
 #include <array>
 #include <iostream>
 
-
 int main()
 {
-	using namespace std;
+	std::array<int, 0> a1;
+	std::array<int, 3> a2;
 
-	array<int, 0> a1;
-	array<int, 3> a2;
-
-	cout << boolalpha;
-	cout << "is a1 empty?  " << a1.empty() << "\n";
-	cout << "is a2 empty?  " << a1.empty() << "\n";
-	cout << "a1.size()     " << a1.size() << "\n";
-	cout << "a2.size()     " << a2.size() << "\n";
-	cout << "a1.max_size() " << a1.max_size() << "\n";
-	cout << "a2.max_size() " << a2.max_size() << "\n";
+	std::cout << std::boolalpha;
+	std::cout << "is a1 empty?  " << a1.empty() << '\n';
+	std::cout << "is a2 empty?  " << a1.empty() << '\n';
+	std::cout << "a1.size()     " << a1.size() << '\n';
+	std::cout << "a2.size()     " << a2.size() << '\n';
+	std::cout << "a1.max_size() " << a1.max_size() << '\n';
+	std::cout << "a2.max_size() " << a2.max_size() << '\n';
 }
